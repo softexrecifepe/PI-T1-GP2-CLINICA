866b3d7bd7deaf8ece30fd680890e2b8fdaf85ca
Leonardo Rafael <119550966+LeonardoRDA1604@users.noreply.github.com>
2024-11-13 11:06:08 -0300

    Update hospitalizations_controller.js

--- controller/hospitalizations_controller.js
+++ controller/hospitalizations_controller.js
@@ -6,7 +6,12 @@ const { pool } = require('../database/dbConnection');
 async function getAllHospitalizedPets (req, res) {
     try {
         const result = await pool.query(
-            `select * from hospitalizations;`
+            `SELECT h.hospitalization_id, h.cage_number, h.reason, h.entry_date, h.requested_exams,
+             h.results_exams, h.hospitalization_observations, h.pet_id, p.pet_name, o.owners_name, o.owners_contact, 
+             h.consultation_id, h.veterinarian_CPF
+            FROM hospitalizations h 
+            JOIN pets p ON h.pet_id = p.pet_id 
+            JOIN pet_owners o ON p.owners_CPF = o.owners_CPF`
         );
         res.json(result.rows);
     } catch (err) {
